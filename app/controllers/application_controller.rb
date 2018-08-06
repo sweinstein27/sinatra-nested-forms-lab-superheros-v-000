9537e825c8a7892898dc2e304ccf2bd49c9f6bb4
Scott Weinstein <sweinstein27@gmail.com>
2018-08-06 15:51:56 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,18 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
+      @team_name = params[:team][:name]
+      @team_motto = params[:team][:motto]
+      @hero_name = []
+      @hero_power = []
+      @hero_bio = []
+      @team_members = params[:team][:members]
+      @team_members.each do |hero|
+        @hero_name << hero[:name]
+        @hero_power << hero[:power]
+        @hero_bio << hero[:bio]
+      end
+
       erb :team
     end
 
